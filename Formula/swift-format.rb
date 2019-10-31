22f58a7a0c5c1987665489533effad1d639d3871
Chris D'Annunzio <chris.dannunzio@leafly.com>
2019-10-31 11:04:51 -0700

    Fix typo in repo names.

--- Formula/swift-format.rb
+++ Formula/swift-format.rb
@@ -1,7 +1,7 @@
 class SwiftFormat < Formula
   desc "Formatting technology for Swift source code"
   homepage "https://github.com/cdann-leafly/swift-format"
-  url "https://github.com/cdann-lealfy/swift-format.git", :branch => "master"
+  url "https://github.com/cdann-leafly/swift-format.git", :branch => "master"
   head "https://github.com/cdann-leafly/swift-format.git", :shallow => false
 
   depends_on :xcode => ["11.0", :build]
